ea09b8e1c3f03bdf3a3a9683420cf03c350178f4
allan <jeanpascallepierre@gmail.com>
2020-09-29 00:23:11 -0400

    29/09/2020
    A faire :
    Redirection
    Fait :
    Gestion des paramètres passer en POST et GET

--- Controller/home.js
+++ Controller/home.js
@@ -1,7 +1,72 @@
+/**
+ * 
+ * @param {*} data Donnée envoyer en POST ou GET
+ */
 exports.Controller = async function Controller(data) {
     var fonctionController = [];
-    let Pages_Site = [];
-    if (data != undefined) {
+    console.log("Data : " + data)
+    let exemple = "Are ya winning son ?";
+    fonctionController.weirdquestion = exemple;
+
+    if (data == undefined || data == "no_data") {
+        /**
+         * @description Use that part to send data to the views
+         */
+        return fonctionController;
+    } else if (data != undefined && data != "no_data") {
+        /**
+         * @description Use that part to process data
+         */
+
+        /**
+         * @argument boolean The use_redirect variable is mean to redirect to a different page choose
+         */
+        fonctionController.use_redirect = true;
+
+        /*
+         * @descriptionThe url_redirect variable is mean to choose the page, give it he name of the controller you want to redirectIf you let it undefined the same page will be render
+         */
+        fonctionController.url_Redirect = "/test1";
+        /**
+         * @description Use that part if des données sont envoyer autant en GET quand POST
+         */
+        let postData;
+        let getData;
+        if (typeof data.get != 'undefined' && typeof data.post != 'undefined') {
+            getData = data.get;
+            postData = data.post;
+
+            nom = getData.slice(0, getData.indexOf("="));
+            let info = getData.slice(getData.indexOf("=") + 1, getData.length);
+            let info_get = { nom, info };
+
+            let data_post = postData.openthedoor;
+
+            fonctionController.dataPost = data_post;
+            fonctionController.dataGet = info_get;
+        }
+        /**
+         * @description Use that part if you want to use POST or GET datas
+         */
+        else if (data) {
+            let info;
+            let nom;
+            if (typeof data.openthedoor == 'string') {
+                nom = "openthedoor";
+                info = data.openthedoor
+            } else {
+                nom = data.slice(0, data.indexOf("="));
+                info = data.slice(data.indexOf("=") + 1, data.length);
+            }
+
+            let info_get = { nom, info };
+
+            let data_post = data;
+
+            fonctionController.dataPost = data_post;
+            fonctionController.dataGet = info_get;
+        }
+
         return fonctionController;
     } else {
         return new Error("Controller error : Aucune informations n'a été envoyer");
--- ServExpress/gestionnaireRequetes.js
+++ ServExpress/gestionnaireRequetes.js
@@ -8,42 +8,81 @@
  * @param {*} nbreq Nombre d'appelle effectuer depuis la mise en router du serveur. Utiliser pour les logs
  */
 
-async function gestionnaireRequetes(request, response, Rviews, Rcontroller, data_get, nbreq) {
-    if(typeof(data_get)!="string")
-    {
+async function gestionnaireRequetes(request, response, Rviews, Rcontroller, Rname_page, data_get, nbreq) {
+    if (Rcontroller && Rviews) {
         console.log("Gestionnaire de Requête : views : " + Rviews + " nbreq:" + nbreq);
         console.log("    ");
         let vue = Rviews;
         let controlleur = Rcontroller;
-        /**
-         * Gestion des requets POST avec data envoyer
-         */
-        if (request._body) {
-            let controllerReq = require("Controller/".controlleur);
-            let Controller = await controllerReq.Controller(request.body);
-    
-            console.log("Gestionnaire de Requête ___-_DATA_-___ : controller : " + Controller);
-            let targetUrl = (typeof(Controller.urlRedirect) != "undefined" ? Controller.urlRedirect : "../");
-            console.log(Controller.fichier_cree)
-            response.redirect(targetUrl);
-            response.end();
-        } 
-        /**
-        * Gestion des requets POST
-        */
-        else if (request._body == undefined || data_get != undefined) {
+        let Controller;
+        if (controlleur != "" || controlleur != undefined) {
             let controllerReq = require("../Controller/" + controlleur + ".js");
-            let Controller = await controllerReq.Controller(data_get);
+            /**
+             * Gestion des requets : POST avec body envoyer sans GET envoyer data_get vide
+             */
+            if (request._body && data_get == undefined) {
+                Controller = await controllerReq.Controller(request.body);
+                console.log("Gestionnaire de Requête ___-_DATA_-___ : controller : " + Controller + " POST");
+                redirection(Controller);
+            }
+            /**
+             * Gestion des requets : POST sans body envoyer et GET envoyer data_get non vide
+             */
+            else if (!request._body && data_get != undefined) {
+                Controller = await controllerReq.Controller(data_get);
+                console.log("Gestionnaire de Requête ___-_DATA_-___ : controller : " + Controller + " GET");
+                redirection(Controller);
+            }
+            /**
+             * Gestion des requets : POST avec body envoyer et GET envoyer data_get non vide
+             */
+            else if (request._body && data_get != undefined) {
+                let controllerReq = require("../Controller/" + controlleur + ".js");
+                /**
+                 * @param {*} Getdata_Postdata
+                 */
+                let datas = { "get": data_get, "post": request.body };
+                Controller = await controllerReq.Controller(datas);
 
-            console.log("Gestionnaire de Requête ____-_RENDER_-____ : controller : " + Controller);
+                console.log("Gestionnaire de Requête ___-_DATA_-___ : controller : " + Controller + " POST & GET");
+                redirection(Controller);
+            }
 
-            response.render("../Views/" + vue + ".ejs", { Controller: Controller, PageTitle: vue });
-            response.end();
+            /**
+             * Gestion des requets POST sans data envoyer
+             */
+            else if (request._body == undefined || data_get == undefined) {
+                data_get = "no_data"
+                let controllerReq = require("../Controller/" + controlleur + ".js");
+                let Controller = await controllerReq.Controller(data_get);
+
+                console.log("Gestionnaire de Requête ____-_RENDER_-____ : controller : " + Controller);
+
+                response.render("../Views/" + vue + ".ejs", { Controller: Controller, PageTitle: Rname_page });
+                response.end();
+            }
+
+            function redirection(controller) {
+                if (controller.use_redirect && controller.url_Redirect != undefined) {
+                    console.log("Gestionnaire de Requête ____-_REDIRECT_-____ : controller : " + controller);
+                    let targetUrl = (typeof(controller.url_Redirect) != "undefined" ? controller.url_Redirect : "/");
+                    response.writeHead(302, {
+                        'Location': targetUrl
+                            //add other headers here...
+                    });
+                    response.end();
+                    //response.redirect(targetUrl);
+                    //response.end();
+                } else if (controller.use_redirect && controller.url_Redirect == undefined || controller.use_redirect == false) {
+                    console.log("Gestionnaire de Requête ____-_RENDER_-____ : controller : " + controller);
+                    response.render("../Views/" + vue + ".ejs", { Controller: controller, PageTitle: Rname_page });
+                    response.end();
+                }
+            }
         }
-    }else
-    {
-        console.log("Hmmm c'est embêtant","Le serveur n'a pas la moindre idée de ce que vous vouliez, étrange non ?")
-        response.render("../ServExpress/erreur/index.ejs", {Message : data_get});
+    } else {
+        console.log("Hmmm c'est embêtant", "Le serveur n'a pas la moindre idée de ce que vous vouliez, étrange non ?")
+        response.render("../ServExpress/erreur/index.ejs", { Message: data_get });
         response.end();
     }
 
