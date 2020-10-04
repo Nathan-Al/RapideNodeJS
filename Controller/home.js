e4d0d70bcf0065482eb3aced6b62d632506a3601
allan <jeanpascallepierre@gmail.com>
2020-10-04 15:51:20 -0300

    04 10 2020
    Nettoyage
    Envoie pour ReadMe purpose
    Changement Controller généré automatiquement.
    Correction des commentaires en anglais
    Changement de la langue des commande an anglais

--- Controller/home.js
+++ Controller/home.js
@@ -1,5 +1,4 @@
-/**
- * 
+/*
  * @param {*} data Donnée envoyer en POST ou GET
  */
 exports.Controller = async function Controller(data) {
@@ -17,46 +16,30 @@ exports.Controller = async function Controller(data) {
 
         return fonctionController;
     } else if (data != undefined && data != "no_data") {
-        /**
+        /*
          * ------------------------------------------
          * @description Use that part to process data
          * ------------------------------------------
          */
-
-        /**
-         * @argument boolean The use_redirect variable is mean to redirect to a different page choose
+        /*
+         * @argument boolean The use_redirect variable is mean to redirect to a different selected page
          */
         fonctionController.use_redirect = false;
-
         /*
-         * @descriptionThe url_redirect variable is mean to choose the page, give it he name of the controller you want to redirectIf you let it undefined the same page will be render
+         * @descriptionThe url_redirect variable is mean to choose the page, give it he name of the controller you want to redirect to. If you let it undefined the same page will be render
          */
         fonctionController.url_Redirect = "/";
-        /**
-         * @description Use that part if des données sont envoyer autant en GET quand POST
+        /*
+         * @description Use that part if datas is send in POST and GET
+         * ! In version 0.0.1 the persistence setting don't work !
          */
-
         fonctionController.persistence = true;
 
-        let postData;
-        let getData;
-        if (typeof data.get != 'undefined' && typeof data.post != 'undefined') {
-            getData = data.get;
-            postData = data.post;
 
-            nom = getData.slice(0, getData.indexOf("="));
-            let info = getData.slice(getData.indexOf("=") + 1, getData.length);
-            let info_get = { nom, info };
-
-            let data_post = postData.openthedoor;
-
-            fonctionController.dataPost = data_post;
-            fonctionController.dataGet = info_get;
-        }
-        /**
+        /*
          * @description Use that part if you want to use POST or GET datas
          */
-        else if (data) {
+        if (data) {
             let info_get;
             let info_post;
 
@@ -73,6 +56,8 @@ exports.Controller = async function Controller(data) {
                 info_get = { nom, info };
                 fonctionController.dataGet = info_get;
             }
+        } else {
+            return new Error("Controller error : aucune informations envoyer");
         }
 
         return fonctionController;
