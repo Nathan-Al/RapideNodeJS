c5b6f2a63f54007741d049170453c01def1715b8
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2022-09-03 20:03:47 -0400

    add app demo

--- main.js
+++ main.js
@@ -4,35 +4,36 @@ const server = new EPS
 /* server.port(3000) */
 
 // set route
-/*
+
 server.route(
     //route1
     {
-        method: "get"
-        path: "/"
-        callback(req,res,next){ res.sen('RapideNodeJS') }
+        method: "get",
+        path: "/",
+        callback(req,res,next){ res.sendFile('index') }
     },
-    //route2
-    {
-        method: "get"
-        path: "/foo"
-        callback(req,res,next){ res.sen('foo') }
-    },
-    //route3
-    {
-        method: "get"
-        path: "/bar"
-        callback(req,res,next){ res.sen('bar') }
-    }
-    // ...
+    // //route2
+    // {
+    //     method: "get",
+    //     path: "/foo",
+    //     callback(req,res,next){ res.send('foo') }
+    // },
+    // //route3
+    // {
+    //     method: "get",
+    //     path: "/bar",
+    //     callback(req,res,next){ res.send('bar') }
+    // }
+    // // ...
 )
-*/
+
 
 // 'app.use'
 /* server.use('public') // accept callback or path */
 
 // 'app.set'
 /* server.set('key', value) */
+app.set('views', 'app');
 
 //app.param (in dev)
 /* server.param('param', (req,res,next,val)=>{}) */
