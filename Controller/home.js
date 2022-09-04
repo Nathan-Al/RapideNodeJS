f453ae5b3e1f550180243880445f174d563e3d6e
Nathan-Al <allan4@outlook.fr>
2022-09-04 00:55:10 +0000

    Add in the controller a test for the database connection Delete useless file

--- Controller/home.js
+++ Controller/home.js
@@ -1,13 +1,16 @@
+const database = require('../ServExpress/service/database/main')
 /**
  * @param {Object} datas The datas send by the user from the view
  * @param {Object} serverParameter Router parameter editable by the user (refere to doc for more detail)
  * @param {Object} serverDatas The server default object to send datas to the view Ex : {name: 'Jhon Does'}
  */
  module.exports.Controller = async function Controller(datas, serverParameter, serverDatas) {
-    let exemple = undefined;
+    let exemple = 'No Exemple';
     if(typeof(datas.Get)!='undefined')
         exemple = "It's nice my son.";
     serverDatas.weirdanswer = exemple;
+    serverDatas.databaseState = await database.verifyConnection()? 'Yes':'No'
 
+    console.log(datas, serverParameter, serverDatas)
     return serverDatas;
 };
