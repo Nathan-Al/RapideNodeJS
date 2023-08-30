da190e1671d97d43aca46e0e098d0e75195170ea
Nathan-Al <allan4@outlook.fr>
2023-08-30 21:25:21 +0000

    Deletting useless Files For The Doc Branch

--- Public/Scripts/home.js
+++
@@ -1,2 +0,0 @@
-let body = document.getElementById('body')
-console.log("Hello from Home !")
--- ServExpress/service/GestiLogs/class/log.js
+++
@@ -1,7 +0,0 @@
-//todo use that classe to define a default object to return log informations
-class Log {
-    constructor () {
-
-    }
-}
-exports.Log = Log
