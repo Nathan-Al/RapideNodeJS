82160b3c19b316a74426da03acbf57d51dfadbe7
Nathan-Al <allan4@outlook.fr>
2025-05-25 14:37:21 +0000

    Update 25 may 2025 - New text - Add the codepen iframe for the tutorial

--- scripts/bulma.js
+++ scripts/bulma.js
@@ -17,9 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
           // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
           el.classList.toggle('is-active');
           $target.classList.toggle('is-active');
-  
         });
       });
     }
-  
   });
